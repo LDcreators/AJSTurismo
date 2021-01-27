09edd63f664292880a099fad7a13ec158a232010
guDaros <gustavo.daros13@gmail.com>
2021-01-27 20:28:03 -0300

    Updating navbar
    Agora muda de cor quando vai rolando o scrol

---
+++ script.js
@@ -0,0 +1,8 @@
+// script para fazer o menu mudar de cor
+(function () {
+    var menu = document.getElementById('menu');
+    window.addEventListener('scroll', function () {
+        if (window.scrollY > 300) menu.classList.add('menuFixo');
+        else menu.classList.remove('menuFixo');
+    });
+})();
