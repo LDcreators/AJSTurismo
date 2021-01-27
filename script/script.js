ac845bb46026fba240dd9a93ccde7acaf370eb41
guDaros <gustavo.daros13@gmail.com>
2021-01-27 20:42:20 -0300

    Folder Organization
    organizei os arquivos que estavam jogados

--- script/script.js
+++ script/script.js
@@ -2,7 +2,7 @@
 (function () {
     var menu = document.getElementById('menu');
     window.addEventListener('scroll', function () {
-        if (window.scrollY > 300) menu.classList.add('menuFixo');
+        if (window.scrollY > 280) menu.classList.add('menuFixo');
         else menu.classList.remove('menuFixo');
     });
 })();
