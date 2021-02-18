3d9dd402437930538de7f70b87dbc92330e91852
Lucas Gabriel <53822214+llucasgvs@users.noreply.github.com>
2021-02-18 14:17:18 -0300

    Arrumado o botão viagens

--- script/script.js
+++ script/script.js
@@ -1,14 +1,16 @@
 // script para fazer o menu mudar de cor
-(function () {
+(function() {
     var menu = document.getElementById('menu');
-    window.addEventListener('scroll', function () {
-        if (window.scrollY > 280) menu.classList.add('menuFixo');
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 280) menu.classList.add('menuScroll');
+        else menu.classList.remove('menuScroll');
+        if (window.scrollY < 280) menu.classList.add('menuFixo');
         else menu.classList.remove('menuFixo');
     });
 })();
 
 //mascarar os input. funciona somente com jQuery
-$(document).ready(function(){
+$(document).ready(function() {
     $('#phone-number').mask('(00) 00000-0000');
-   
-  });
+
+});
